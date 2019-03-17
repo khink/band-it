4f3c1336c378118622280bac3998c365a5e88ded
Kees Hink <keeshink@gmail.com>
2019-03-17 19:38:59 +0100

    Allow our frontend to read our backend

--- bandapp_backend/bandapp_backend/settings.py
+++ bandapp_backend/bandapp_backend/settings.py
@@ -39,9 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'songs.apps.SongsConfig',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Development
+CORS_ORIGIN_ALLOW_ALL = True
