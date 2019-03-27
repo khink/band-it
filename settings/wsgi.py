b2f704861f74b66dc7aceaa76925651e51fdc935
Kees Hink <keeshink@gmail.com>
2019-03-27 19:32:52 +0100

    Move everything up one level (remove directory)
    Also, remove superfluous and incorrect references to Django 1.10 docs.
    The project was bootstrapped for Django 2.1.7 for Django 2.1.7.

--- settings/wsgi.py
+++ settings/wsgi.py
@@ -2,9 +2,6 @@
 WSGI config for bandapp_backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
